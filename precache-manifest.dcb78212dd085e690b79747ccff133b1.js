42a5ee6572dd907a2cdf0c4cc21fc01a52e74cd8
Christian Carandang <91305708+christiandc19@users.noreply.github.com>
2022-10-25 00:09:39 -0700

    Updates

---
+++ precache-manifest.dcb78212dd085e690b79747ccff133b1.js
@@ -0,0 +1,178 @@
+self.__precacheManifest = [
+  {
+    "revision": "9ef3a241d29e9093284a",
+    "url": "/sacramento-rehab/static/css/main.ef788129.chunk.css"
+  },
+  {
+    "revision": "9ef3a241d29e9093284a",
+    "url": "/sacramento-rehab/static/js/main.14eb5a14.chunk.js"
+  },
+  {
+    "revision": "60dc47d8d7088a94abc6",
+    "url": "/sacramento-rehab/static/js/runtime~main.5444f2fc.js"
+  },
+  {
+    "revision": "904a3f2116c36db9ea21",
+    "url": "/sacramento-rehab/static/js/2.1c5f3793.chunk.js"
+  },
+  {
+    "revision": "7b76a1c0e9e04925b1890d648d4be726",
+    "url": "/sacramento-rehab/static/media/logo-wh.7b76a1c0.png"
+  },
+  {
+    "revision": "6d425e638294d27db7bacfbc38a5426b",
+    "url": "/sacramento-rehab/static/media/aetna.6d425e63.png"
+  },
+  {
+    "revision": "b1a115cb567510e8ec14e6b05ef8bdde",
+    "url": "/sacramento-rehab/static/media/beacon.b1a115cb.png"
+  },
+  {
+    "revision": "a685f0cda3e3d8e2f530c9ce30865aa5",
+    "url": "/sacramento-rehab/static/media/bluecross.a685f0cd.png"
+  },
+  {
+    "revision": "6defc1965a08a859708e60937009ca63",
+    "url": "/sacramento-rehab/static/media/cigna.6defc196.png"
+  },
+  {
+    "revision": "f64380adb288f823da65111d09c715c9",
+    "url": "/sacramento-rehab/static/media/mhn.f64380ad.png"
+  },
+  {
+    "revision": "726f644e55b1610ea4792c5d89b57bff",
+    "url": "/sacramento-rehab/static/media/molina-healthcare.726f644e.png"
+  },
+  {
+    "revision": "caa531b2f260498491cb708dfef0f67f",
+    "url": "/sacramento-rehab/static/media/united-healthcare.caa531b2.png"
+  },
+  {
+    "revision": "7d58648fe12f32fcbeaff28139c223e5",
+    "url": "/sacramento-rehab/static/media/hero-vbg2.7d58648f.mp4"
+  },
+  {
+    "revision": "3440ad860a5eb96efa01d65c136aecc3",
+    "url": "/sacramento-rehab/static/media/mental-illness.3440ad86.png"
+  },
+  {
+    "revision": "fbeb56fb1286ccea01afc386877ac4fa",
+    "url": "/sacramento-rehab/static/media/substance-use.fbeb56fb.png"
+  },
+  {
+    "revision": "e43d2b5528c1d0503918199e532fceaa",
+    "url": "/sacramento-rehab/static/media/addiction-treatment.e43d2b55.png"
+  },
+  {
+    "revision": "2cf97747278a8f166be1d8a3c958eb0c",
+    "url": "/sacramento-rehab/static/media/employment-assistance.2cf97747.png"
+  },
+  {
+    "revision": "5a151fdeb4aad39f5816bfd712ffb828",
+    "url": "/sacramento-rehab/static/media/home-image1.5a151fde.jpg"
+  },
+  {
+    "revision": "3b1f2acd8a1eadd948e097e998932e65",
+    "url": "/sacramento-rehab/static/media/treatment-image-1.3b1f2acd.jpg"
+  },
+  {
+    "revision": "d4ce7171a4e1bdf8636293c1c628b1d9",
+    "url": "/sacramento-rehab/static/media/activity.d4ce7171.jpg"
+  },
+  {
+    "revision": "c892ec74a0db3399d82c7be2ccc2b384",
+    "url": "/sacramento-rehab/static/media/therapy.c892ec74.jpg"
+  },
+  {
+    "revision": "38b750562d3162d593836104fe332944",
+    "url": "/sacramento-rehab/static/media/staff.38b75056.jpg"
+  },
+  {
+    "revision": "da98d45a05a0da0f0e4d27a2b66ea76f",
+    "url": "/sacramento-rehab/static/media/clinic.da98d45a.jpg"
+  },
+  {
+    "revision": "3dc594eb44086fb3004af0b556a15818",
+    "url": "/sacramento-rehab/static/media/employment-assistance2.3dc594eb.png"
+  },
+  {
+    "revision": "3b35ae449e240b2ccc55c2a5c8d35617",
+    "url": "/sacramento-rehab/static/media/team-image-1.3b35ae44.jpg"
+  },
+  {
+    "revision": "e3036db9350d49f97d7bc61110def10b",
+    "url": "/sacramento-rehab/static/media/team-image-2.e3036db9.jpg"
+  },
+  {
+    "revision": "113683f2ab207479764bdd1181154768",
+    "url": "/sacramento-rehab/static/media/team-image-3.113683f2.jpg"
+  },
+  {
+    "revision": "6cec3afc7fb7fc7e27492eeada612118",
+    "url": "/sacramento-rehab/static/media/team-image-4.6cec3afc.jpg"
+  },
+  {
+    "revision": "30ffc312a1529d5117e94163c4ba03ac",
+    "url": "/sacramento-rehab/static/media/team-image-5.30ffc312.jpg"
+  },
+  {
+    "revision": "69b4dbd7ed1033288e306d109f39cee1",
+    "url": "/sacramento-rehab/static/media/team-image-6.69b4dbd7.jpg"
+  },
+  {
+    "revision": "8b32ca6894075ccf463616f504c99ab2",
+    "url": "/sacramento-rehab/static/media/ross.8b32ca68.jpg"
+  },
+  {
+    "revision": "8cffd4d9db5d362bbb2e1425565c910d",
+    "url": "/sacramento-rehab/static/media/rachel2.8cffd4d9.jpg"
+  },
+  {
+    "revision": "860e1fe71047cf9abb45d2cdc0d9a332",
+    "url": "/sacramento-rehab/static/media/joey.860e1fe7.jpg"
+  },
+  {
+    "revision": "6fea533b3e5d368852d380f69eed96cb",
+    "url": "/sacramento-rehab/static/media/monica.6fea533b.jpg"
+  },
+  {
+    "revision": "acfc4e17ab9612f0293b74391027311b",
+    "url": "/sacramento-rehab/static/media/chandler.acfc4e17.jpg"
+  },
+  {
+    "revision": "824ca03ed1c8be62014f43ca59cc2f2d",
+    "url": "/sacramento-rehab/static/media/footer-bg.824ca03e.jpg"
+  },
+  {
+    "revision": "4c2e3da2f467565337e2cc341ce87da2",
+    "url": "/sacramento-rehab/static/media/hero-bg2.4c2e3da2.jpg"
+  },
+  {
+    "revision": "88bf332580497460588477b1dd86110a",
+    "url": "/sacramento-rehab/static/media/treatment-bg2.88bf3325.jpg"
+  },
+  {
+    "revision": "4cc2bc74f57da461e48621466429f611",
+    "url": "/sacramento-rehab/static/media/mission-bg.4cc2bc74.jpg"
+  },
+  {
+    "revision": "3944e199a8823c25248d2269b9554a2c",
+    "url": "/sacramento-rehab/static/media/about-us-bg.3944e199.jpg"
+  },
+  {
+    "revision": "130f85eb352a9fa72e76245ec5afd069",
+    "url": "/sacramento-rehab/static/media/insurance-bg.130f85eb.jpg"
+  },
+  {
+    "revision": "b91f325701ff7c472c1c955a92cbcd01",
+    "url": "/sacramento-rehab/static/media/contact-bg.b91f3257.jpg"
+  },
+  {
+    "revision": "f9fef6f88248771ceeb072c7625177b9",
+    "url": "/sacramento-rehab/static/media/testimonials-bg.f9fef6f8.jpg"
+  },
+  {
+    "revision": "9567315544896b0f89ef274cf7d0e7a6",
+    "url": "/sacramento-rehab/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/sacramento-rehab/precache-manifest.5315d25e0c6f1dca182012cfbba2a0fe.js"
+  "/sacramento-rehab/precache-manifest.dcb78212dd085e690b79747ccff133b1.js"
 );
 
 workbox.clientsClaim();
