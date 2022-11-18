c5520bcc47b77c5ad4afadff04e34f81ab2198a6
Christian Carandang <91305708+christiandc19@users.noreply.github.com>
2022-11-17 20:16:10 -0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/sacramento-rehab/precache-manifest.d1b29f3a11543c9d8dda460fd2c3ef5b.js"
+  "/sacramento-rehab/precache-manifest.7334a1454dbe2aebf55c1a010963ce3e.js"
 );
 
 workbox.clientsClaim();
